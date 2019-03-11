a4e9259d32f5b40acef70f93db13a40f08fa383d
Jakub Dąbek <jakub.dabek@gmail.com>
2019-03-11 11:34:29 +0100

    Add standard deviation calculation, more const

--- lab1/main.c
+++ lab1/main.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 
 int LIST_SIZE = 100;
@@ -16,7 +17,7 @@ void fill_random(struct forward_list **head, int list[], size_t size);
 void fill_ascending(struct forward_list **head, int list[], size_t size);
 void fill_descending(struct forward_list **head, int list[], size_t size);
 
-struct fill_function fill_functions[] =
+const struct fill_function fill_functions[] =
 {
     { "random", fill_random },
     { "ascending", fill_ascending },
@@ -29,7 +30,7 @@ struct find_function
     struct forward_list** (*find)(struct forward_list**, int);
 };
 
-struct find_function find_functions[] =
+const struct find_function find_functions[] =
 {
     { "findMTF", findMTF },
     { "findTRANS", findTRANS },
@@ -38,14 +39,14 @@ struct find_function find_functions[] =
 struct find_max_function
 {
     const char *description;
-    int(*find_max)(struct forward_list**, struct find_function);
+    int(*find_max)(struct forward_list**, const struct find_function*);
 };
 
-int find_max_ascending(struct forward_list **head, struct find_function find_func);
-int find_max_descending(struct forward_list **head, struct find_function find_func);
-int find_max_random(struct forward_list **head, struct find_function find_func);
+int find_max_ascending(struct forward_list **head, const struct find_function *find_func);
+int find_max_descending(struct forward_list **head, const struct find_function *find_func);
+int find_max_random(struct forward_list **head, const struct find_function *find_func);
 
-struct find_max_function find_max_functions[] =
+const struct find_max_function find_max_functions[] =
 {
     { "ascending", find_max_ascending },
     { "descending", find_max_descending },
@@ -53,10 +54,18 @@ struct find_max_function find_max_functions[] =
 };
 
 void test(
-    struct fill_function fill_func,
-    struct find_max_function find_max_func,
-    struct find_function find_func
+    const struct fill_function *fill_func,
+    const struct find_max_function *find_max_func,
+    const struct find_function *find_func
 );
+
+void update_mean_variance(const int count, double * const mean, double * const M2, const int newValue)
+{
+    const double delta = newValue - *mean;
+    *mean += delta / (count + 1);
+    const double delta2 = newValue - *mean;
+    *M2 += delta * delta2;
+}
 
 int main(int argc, char *argv[])
 {
@@ -71,33 +80,37 @@ int main(int argc, char *argv[])
         {
             for (int k = 0; k < sizeof(find_functions) / sizeof(find_functions[0]); k++)
             {
-                double sum_find_comparisons = 0.0;
-                double sum_delete_comparisons = 0.0;
-                const int num_iters = 1000;
-                struct fill_function fill_func = fill_functions[j];
-                struct find_max_function find_max_func = find_max_functions[i];
-                struct find_function find_func = find_functions[k];
+                const struct fill_function *fill_func = &fill_functions[j];
+                const struct find_max_function *find_max_func = &find_max_functions[i];
+                const struct find_function *find_func = &find_functions[k];
                 printf("\nTesting %s with fill %s and find_max %s\n",
-                        find_func.description,
-                        fill_func.description,
-                        find_max_func.description);
+                        find_func->description,
+                        fill_func->description,
+                        find_max_func->description);
+                
+                double mean_find_comparisons = 0.0;
+                double M2_find_comparisons = 0.0;
+                double mean_delete_comparisons = 0.0;
+                double M2_delete_comparisons = 0.0;
+                const int num_iters = 1000;
                 for (int iters = 0; iters < num_iters; iters++)
                 {
                     find_comparisons_count = 0;
                     delete_comparisons_count = 0;
                     test(fill_func, find_max_func, find_func);
 
-                    sum_find_comparisons += find_comparisons_count;
-                    sum_delete_comparisons += delete_comparisons_count;
+                    update_mean_variance(iters, &mean_find_comparisons, &M2_find_comparisons, find_comparisons_count);
+                    update_mean_variance(iters, &mean_delete_comparisons, &M2_delete_comparisons, delete_comparisons_count);
                 }
-                printf("find comparisons: %.2lf, delete comparisons: %.2lf\n",
-                        sum_find_comparisons / num_iters, sum_delete_comparisons / num_iters);
+                printf("find comparisons: %8.2lf, stddev: %10.4lf, delete comparisons: %8.2lf, stddev: %10.4lf\n",
+                        mean_find_comparisons, sqrt(M2_find_comparisons / num_iters),
+                        mean_delete_comparisons, sqrt(M2_delete_comparisons / num_iters));
             }
         }
     }
 }
 
-void shuffle(int arr[], size_t n)
+void shuffle(int arr[], const size_t n)
 {
     if (n > 1) 
     {
@@ -111,7 +124,7 @@ void shuffle(int arr[], size_t n)
     }
 }
 
-void fill_random(struct forward_list **head, int list[], size_t size)
+void fill_random(struct forward_list **head, int list[], const size_t size)
 {
     for (size_t i = 0; i < size; i++)
     {
@@ -125,7 +138,7 @@ void fill_random(struct forward_list **head, int list[], size_t size)
     }
 }
 
-void fill_ascending(struct forward_list **head, int list[], size_t size)
+void fill_ascending(struct forward_list **head, int list[], const size_t size)
 {
     for (int i = size - 1; i >= 0; i--)
     {
@@ -133,7 +146,7 @@ void fill_ascending(struct forward_list **head, int list[], size_t size)
     }
 }
 
-void fill_descending(struct forward_list **head, int list[], size_t size)
+void fill_descending(struct forward_list **head, int list[], const size_t size)
 {
     for (size_t i = 0; i < size; i++)
     {
@@ -142,12 +155,12 @@ void fill_descending(struct forward_list **head, int list[], size_t size)
 }
 
 
-int find_max_ascending(struct forward_list **head, struct find_function find_func)
+int find_max_ascending(struct forward_list **head, const struct find_function * const find_func)
 {
     int max = *get_value(*head, 0);
     for (size_t i = 0; i < LIST_SIZE; i++)
     {
-        if (find_func.find(head, i) != NULL && i > max)
+        if (find_func->find(head, i) != NULL && i > max)
         {
             max = i;
         }
@@ -155,12 +168,12 @@ int find_max_ascending(struct forward_list **head, struct find_function find_fun
     return max;
 }
 
-int find_max_descending(struct forward_list **head, struct find_function find_func)
+int find_max_descending(struct forward_list **head, const struct find_function * const find_func)
 {
     int max = *get_value(*head, 0);
     for (int i = LIST_SIZE - 1; i >= 0; i--)
     {
-        if (find_func.find(head, i) != NULL && i > max)
+        if (find_func->find(head, i) != NULL && i > max)
         {
             max = i;
         }
@@ -168,7 +181,7 @@ int find_max_descending(struct forward_list **head, struct find_function find_fu
     return max;
 }
 
-int find_max_random(struct forward_list **head, struct find_function find_func)
+int find_max_random(struct forward_list **head, const struct find_function * const find_func)
 {
     int list[LIST_SIZE];
     for (size_t i = 0; i < LIST_SIZE; i++)
@@ -180,7 +193,7 @@ int find_max_random(struct forward_list **head, struct find_function find_func)
     int max = *get_value(*head, 0);
     for (size_t i = 0; i < LIST_SIZE; i++)
     {
-        if (find_func.find(head, list[i]) != NULL && list[i] > max)
+        if (find_func->find(head, list[i]) != NULL && list[i] > max)
         {
             max = list[i];
         }
@@ -191,18 +204,18 @@ int find_max_random(struct forward_list **head, struct find_function find_func)
 
 
 void test(
-    struct fill_function fill_func,
-    struct find_max_function find_max_func,
-    struct find_function find_func
+    const struct fill_function * const fill_func,
+    const struct find_max_function * const find_max_func,
+    const struct find_function * const find_func
 )
 {
     struct forward_list *head = NULL;
     int values[LIST_SIZE];
-    fill_func.fill(&head, values, LIST_SIZE);
+    fill_func->fill(&head, values, LIST_SIZE);
     while (!is_empty(head))
     {
         //printf("Before find_max: "); print_forward_list(head); printf("\n");
-        int max = find_max_func.find_max(&head, find_func);
+        int max = find_max_func->find_max(&head, find_func);
         //printf("Before delete: "); print_forward_list(head); printf("\n");
         delete_value(&head, max);
     }
